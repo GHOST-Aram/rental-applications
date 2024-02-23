2aae17d2223902355d9b1888a494a21ea8a3a030
GHOST-Aram <owinoojala@gmail.com>
2024-02-23 11:32:20 +0300

    chore: Mock methods in DataAccess class

--- src/data-access/mock-data-access.ts
+++ src/data-access/mock-data-access.ts
@@ -1,8 +1,57 @@
-import { DataAccess } from "./data-access";
-import { ApplicationSubmissionModel } from "./model";
+import { GenericDataAccess } from "../z-library/bases/generic-data-access";
+import { 
+        ApplicationSubmission, 
+        ApplicationSubmissionModel,
+        HydratedApplicationSub
+} from "./model";
+import { postData } from "../test/data/data";
+import { Paginator } from "../z-library/HTTP/http-response";
 
-export class MockDataAccess extends DataAccess{
+export class DataAccess extends GenericDataAccess
+<ApplicationSubmissionModel, ApplicationSubmission>{
     constructor(model: ApplicationSubmissionModel){
         super(model)
     }
+    public createNew = jest.fn(async(data: ApplicationSubmission
+        ): Promise<HydratedApplicationSub>=>{
+        return new ApplicationSubmission(data)
+    })
+
+    public findByReferenceId =  jest.fn(async(refId: string
+        ): Promise<HydratedApplicationSub| null> =>{
+        if(refId === '64c9e4f2df7cc072af2ac9e0')
+            return new ApplicationSubmission(postData)
+
+        return null
+    })
+
+    public findWithPagination = jest.fn(async(paginator: Paginator
+        ): Promise<HydratedApplicationSub[]> =>{
+        return generateFakeDocs(paginator.limit)
+    })
+
+    public findByIdAndUpdate = jest.fn(async(assetId: string, updateDoc: ApplicationSubmission
+        ): Promise<HydratedApplicationSub | null> =>{
+        if(assetId === '64c9e4f2df7cc072af2ac9e4' )
+            return new ApplicationSubmission(updateDoc)
+        return null
+    })
+
+    public findByIdAndDelete = jest.fn(async(assetId: string
+        ): Promise<HydratedApplicationSub | null> =>{
+        if(assetId === '64c9e4f2df7cc072af2ac9e4' )
+            return new ApplicationSubmission(postData)
+        return null
+    })
+}
+
+const generateFakeDocs = (limit: number): HydratedApplicationSub[] =>{
+    const docs: HydratedApplicationSub[] = []
+
+    while(limit > 0){
+        docs.push(new ApplicationSubmission(postData))
+        limit --
+    }
+
+    return docs
 }
