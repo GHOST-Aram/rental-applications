0f13b69086bd4ff9e6736de421cf88ded2a4727c
GHOST-Aram <owinoojala@gmail.com>
2024-02-23 08:06:01 +0300

    feat: Create Controller class
         - Extend generic controller class

---
+++ src/controllers/controller.ts
@@ -0,0 +1,7 @@
+import { DataAccess } from '../data-access/data-access';
+import { GenericController } from '../z-library/bases/generic-controller'
+export class Controller extends GenericController<DataAccess>{
+    constructor(dataAccess: DataAccess){
+        super(dataAccess)
+    }
+}
